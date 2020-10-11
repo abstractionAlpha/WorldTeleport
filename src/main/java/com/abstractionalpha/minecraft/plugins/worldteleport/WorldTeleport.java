bd2751e148e1bcda5f98f890005b7d0f0dd4edf0
unknown <mhicks071@yahoo.com>
2020-10-11 16:40:51 -0400

    Hash maps for master command

--- src/main/java/com/abstractionalpha/minecraft/plugins/worldteleport/WorldTeleport.java
+++ src/main/java/com/abstractionalpha/minecraft/plugins/worldteleport/WorldTeleport.java
@@ -1,17 +1,48 @@
 package com.abstractionalpha.minecraft.plugins.worldteleport;
 
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class WorldTeleport extends JavaPlugin {
 
+    /**
+     * Plugin start-up logic.
+     */
     @Override
     public void onEnable() {
         // Plugin startup logic
 
     }
 
+    /**
+     * Plugin shut-down logic.
+     */
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        if (cmd.getName().equalsIgnoreCase("tpw")) {
+            // tpw <world> <x> <y> <z>
+            // tpw <usr> <world> <x> <y> <z>
+
+            switch(args.length) {
+                case 4:
+                    sender.sendMessage("tpw <world> <x> <y> <z>");
+                    break;
+                case 5:
+                    sender.sendMessage("tpw <usr> <world> <x> <y> <z>");
+                    break;
+                default:
+                    sender.sendMessage("Args length: " + args.length);
+                    break;
+            }
+        }
+
+        return true;
+
     }
 }
